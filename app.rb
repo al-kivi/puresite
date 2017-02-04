b3b142892144b47d8f8559ebb37f283929703b03
al_kivi <akivi@ica.net>
2017-02-04 15:11:37 +0000

    Store the completed application

--- app.rb
+++ app.rb
@@ -1,5 +1,24 @@
-# app.rb Basic example to test environment
+# app.rb Chapter 6 ... Multi-Page application
 require 'sinatra'
-get '/' do
-  'Hello world!'
+
+def menu_tag(href)
+  if request.path_info == href
+    " \"pure-menu-item pure-menu-selected\" "
+  else  
+    " \"pure-menu-item\" "
+  end
 end
+
+get '/' do
+  @image = "images/heroshot.jpg"
+  erb :home, :layout => :layout2
+end
+
+get '/tour' do
+  erb :ribbon, :layout => :layout2
+end
+
+get '/contact' do
+  erb :contact, :layout => :layout2
+end
+    
